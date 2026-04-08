c517660f17cc9cc3167731aa51c68ac2d3717ff6
Murilo Ferraz <mumufeferraz2002@gmail.com>
2026-04-08 13:05:01 -0300

    add host

--- server.js
+++ server.js
@@ -70,5 +70,6 @@ server.delete('/videos/:id', async (request, reply) => {
 })
 
 server.listen({
+    host: '0.0.0.0',
     port: process.env.PORT ?? 3333, 
 }); //Fastify designa portas atraves de objetos, entao é necessario chamar o listen como objeto e passar a porta por dentro ele
